67b636236a7aa30b1d825310d1342de34bb793e1
aj1m0n <surpasstdu@gmail.com>
2021-08-20 12:11:46 +0900

    list -> dict

--- api.py
+++ api.py
@@ -6,32 +6,14 @@ import os
 app = flask.Flask(__name__)
 data_path = "./data/"
 
-
-@app.route('/images/<image_id>')
-def send_image(image_id):
-    f = open(data_path + 'images/' + str(image_id) + '.jpg', 'rb')
-    image = f.read()
-    return Response(response=image, content_type='image/png')
-
 @app.route('/data')
 def send_data():
-    _i = 1 
-    _j = 0
-    id_list = []
-    date_list = []
-    direction_list = []
-    json_data = {}
+    json_list = []
     files = os.listdir(data_path + 'json/')
     for _file in files:
         json_open = json.load(open(data_path + 'json/' + _file, 'r'))
-        id_list.append(json_open["id"])
-        date_list.append(json_open["date"])
-        direction_list.append(json_open["direction"])
-        
-    json_data["id_list"] = id_list
-    json_data["date_list"] = date_list
-    json_data["direction"] = direction_list
-    return jsonify(json_data)
+        json_list.append(json_open)
+    return jsonify(json_list)
 
 
 @app.route('/')
