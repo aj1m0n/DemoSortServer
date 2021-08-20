cbaca30a728d8065113371d44948cec45520055d
aj1m0n <surpasstdu@gmail.com>
2021-08-20 13:01:51 +0900

    update

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,40 @@
 const timer = 5000;
-window.addEventListener('load', function() {
-	setInterval('location.reload()',timer);
+window.addEventListener('load', function () {
+	// setInterval('location.reload()', timer);
 	getDataList();
-      });
-
+});
+var sort_by = function (field, reverse, primer) {
+	reverse = (reverse) ? -1 : 1;
+	return function (a, b) {
+		a = a[field];
+		b = b[field];
+		if (typeof (primer) != 'undefined') {
+			a = primer(a);
+			b = primer(b);
+		}
+		if (a < b) return reverse * -1;
+		if (a > b) return reverse * 1;
+		return 0;
+	}
+}
 function getDataList() {
 	var url = '/data';
 	fetch(url)
-	.then(function (data) {
-		return data.json(); // 読み込むデータをJSONに設定
-	      })
-	.then(function (json) {
-		var id_list = json.id_list;
-		var data_list = json.date_list;
-		var direction_list = json.direction;
+		.then(function (data) {
+			return data.json(); // 読み込むデータをJSONに設定
+		})
+		.then(function (json) {
+			// json.sort(sort_by('id', false, function(a){return a.toUpperCase()}));
+			for (var i = 0; i < json.length; i++) {
+                var id = json[i].id;
+                var date = json[i].date;
+                var direction = json[i].direction;
 
-		for (var i = 0; i < json.id_list.length; i++){
-			var row = document.getElementById('id-list').insertRow();
-			row.insertCell().textContent = id_list[i];
-			row.insertCell().textContent = data_list[i];
-			row.insertCell().textContent = direction_list[i];
-		}
-	});
+				var row = document.getElementById('id-list').insertRow();
+				row.insertCell().textContent = id;
+				row.insertCell().textContent = date;
+				row.insertCell().textContent = direction;
+			}
+		});
+}
 
-	
-	}
